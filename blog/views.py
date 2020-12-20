b4aff1487ac1291f29aae79ca1dd8d94a124e54a
artemtk <art@arcbyt.com>
2020-12-20 14:54:06 -0800

    Custom tags

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
-from .forms import EmailPostForm
-from .models import Post
+from django.db.models import Count
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
+from taggit.models import Tag
 
 
 def post_share(request, post_id):
@@ -31,11 +33,18 @@ def post_share(request, post_id):
                    'sent': sent})
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     posts = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        posts = posts.filter(tags__in=[tag])
+
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -44,6 +53,34 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags',  '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
